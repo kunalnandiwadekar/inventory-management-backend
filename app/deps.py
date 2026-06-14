c447ea24099c796a0a4b21b32477075f8f66afd1
Kunalun772 <149257532+kunalnandiwadekar@users.noreply.github.com>
2026-06-14 17:55:27 +0530

    Add user authentication with JWT and HTTPBearer

--- app/deps.py
+++ app/deps.py
@@ -1,4 +1,12 @@
 from app.database import SessionLocal
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError
+
+SECRET_KEY = "secretkey123"
+ALGORITHM = "HS256"
+
+security = HTTPBearer()
 
 def get_db():
     db = SessionLocal()
@@ -6,3 +14,16 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    try:
+        payload = jwt.decode(credentials.credentials, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return username
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token"
+        )
